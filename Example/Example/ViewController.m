7cb807bfbc3bfc9ffcf5fae97ced69d94c2c260c
王顺 <360752546@qq.com>
2015-06-11 23:01:34 +0800

    暂存天气地址

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    /*
+     http://route.showapi.com/9-2?showapi_appid=187&showapi_sign=simple_11be041a6d864c849b7e17d75ec663d3%20&areaid=101291401&showapi_timestamp=2014-11-14%2014:22:39
+     */
+    
 }
 
 - (void)didReceiveMemoryWarning {
